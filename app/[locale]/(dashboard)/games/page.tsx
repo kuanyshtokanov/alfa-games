c1e5ce16b505ff8546113d4c171f211efe0704e4
Darkhan Iskakov <darkhan@Mac.lan>
2026-01-25 22:47:35 +0200

    Join event button fix

--- app/[locale]/(dashboard)/games/page.tsx
+++ app/[locale]/(dashboard)/games/page.tsx
@@ -286,7 +286,7 @@ export default function FindMatchPage() {
                   onAction={() =>
                     isRegistered
                       ? handleCancelEvent(game.id)
-                      : handleJoinEvent(game.id)
+                      : router.push(`/games/${game.id}`)
                   }
                   actionLabel={
                     isRegistered
